616518e1e556be3b1427879726547459a826677a
juice <moses@thevinsons.com>
2019-03-04 10:50:59 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('message', function(message) {
+         wss.clients.forEach((client) => {
+    client.send(message);
+  });
+    });
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+// setInterval(() => {
+//   wss.clients.forEach((client) => {
+//     client.send(new Date().toTimeString());
+//   });
+// }, 1000);
+
+wss.on('message', function incoming(data) {
+  wss.clients.forEach((client) => {
+    client.send('hello');
+  });
+});
+
+wss.on('event', function incoming(data) {
+  wss.clients.forEach((client) => {
+    client.send('hello');
+  });
+});
